5c8d749dc21e2b39cbb69e14c782489bdcf6cc41
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-08-23 10:11:08 +0300

    my answers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let kilwe="Ayuub"
+console.log(kilwe)
+
+let fikrcamp="Bootcamp"
+console.log(fikrcamp)
+
+
+x=20
+y=19
+if(x > y)
+{
+    console.log("The largest number out of the two numbers is X")
+} else if(x < y){
+    console.log("The largest number out of the two numbers is y")
+}
